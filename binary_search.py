46fadea7a88c7e969279325fe789d4b275eec484
Juraj Siroky <siroky.juraj0@gmail.com>
2024-10-13 00:34:27 +0200

    edited solution for problem 704

--- binary_search.py
+++ binary_search.py
@@ -1,12 +1,12 @@
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
-        low, high = 0, len(nums)
-        while low < high:
-            mid = (low + high) // 2
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
             if nums[mid] > target:
-                high = mid
+                right = mid - 1
             else:
-                low = mid + 1
+                left = mid + 1
         return -1
